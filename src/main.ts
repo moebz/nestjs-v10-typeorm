843e246c8385e2c59ac9bf6a8a9adffc1e08c01d
Moises B <mbaez091@gmail.com>
2023-10-23 21:49:48 -0300

    read port from .env

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService);
+
+  const PORT = configService.get('PORT');
+
+  console.log('main.bootstrap.PORT', PORT);
 
   const config = new DocumentBuilder()
     .setTitle('API')
@@ -13,6 +20,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
-  await app.listen(3000);
+  await app.listen(PORT);
 }
 bootstrap();
