fbcc9b08306f00e43c9c1ef189c3b1dd4ae81609
Moises B <mbaez091@gmail.com>
2023-10-29 18:44:56 -0300

    generate jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,9 +4,26 @@ import { UsersModule } from 'src/users/users.module';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './strategies/local.strategy';
 import { AuthController } from './controllers/auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigType } from '@nestjs/config';
+import config from 'src/config';
 
 @Module({
-  imports: [UsersModule, PassportModule],
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.registerAsync({
+      inject: [config.KEY],
+      useFactory: (configService: ConfigType<typeof config>) => {
+        return {
+          secret: configService.jwtSecret,
+          signOptions: {
+            expiresIn: '10d',
+          },
+        };
+      },
+    }),
+  ],
   providers: [AuthService, LocalStrategy],
   controllers: [AuthController],
 })
